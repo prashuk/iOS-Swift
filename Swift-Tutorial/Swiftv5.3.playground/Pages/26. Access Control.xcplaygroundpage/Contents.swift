fc058f62ae31df5d6dde2cf5a4555358824b6cba
Prashuk <prashuk.ajmera@gmail.com>
2020-11-22 15:34:45 -0600

    Access Control part 1

--- Swift-Tutorial/Swiftv5.3.playground/Pages/26. Access Control.xcplaygroundpage/Contents.swift
+++ Swift-Tutorial/Swiftv5.3.playground/Pages/26. Access Control.xcplaygroundpage/Contents.swift
@@ -1,1 +1,12 @@
 import Foundation
+
+// Access control restricts access to parts of your code from code in other source files and modules. This feature enables you to hide the implementation details of your code, and to specify a preferred interface through which that code can be accessed and used.
+// You can assign specific access levels to individual types (classes, structures, and enumerations), as well as to properties, methods, initializers, and subscripts belonging to those types. Protocols can be restricted to a certain context, as can global constants, variables, and functions.
+
+// A module is a single unit of code distribution—a framework or application that is built and shipped as a single unit and that can be imported by another module with Swift’s import keyword.
+
+// A source file is a single Swift source code file within a module (in effect, a single file within an app or framework). Although it’s common to define individual types in separate source files, a single source file can contain definitions for multiple types, functions, and so on.
+
+// Access Levels
+// These access levels are relative to the source file in which an entity is defined, and also relative to the module that source file belongs to.
+
