a17e10929475b1be687cec5f50d7358961f3fdae
Prashuk Ajmera <prashuk.ajmera@gmail.com>
2020-12-29 11:44:44 -0600

    Basic Operators

---
+++ Swiftv5.3.playground/Pages/2. Basic Operators.xcplaygroundpage/Contents.swift
@@ -0,0 +1,65 @@
+import Foundation
+
+// Nil-Coalescing Operator
+// The nil-coalescing operator (a ?? b) unwraps an optional a if it contains a value, or returns a default value b if a is nil.
+// a != nil ? a! : b
+
+let defaultColorName = "red"
+var userDefinedColorName: String?   // defaults to nil
+
+var colorNameToUse = userDefinedColorName ?? defaultColorName
+// userDefinedColorName is nil, so colorNameToUse is set to the default of "red"
+
+
+
+// Range Operators
+// Closed Range Operator (a...b)
+// Runs from a to b, and includes the values a and b. The value of a must not be greater than b.
+for index in 1...5 {
+    print("\(index) times 5 is \(index * 5)")
+}
+// 1 times 5 is 5
+// 2 times 5 is 10
+// 3 times 5 is 15
+// 4 times 5 is 20
+// 5 times 5 is 25
+
+
+// Half-Open Range Operator (a..<b)
+// Runs from a to b, but doesn’t include b. It’s said to be half-open because it contains its first value, but not its final value. As with the closed range operator, the value of a must not be greater than b. If the value of a is equal to b, then the resulting range will be empty.
+let names = ["Anna", "Alex", "Brian", "Jack"]
+let count = names.count
+for i in 0..<count {
+    print("Person \(i + 1) is called \(names[i])")
+}
+// Person 1 is called Anna
+// Person 2 is called Alex
+// Person 3 is called Brian
+// Person 4 is called Jack
+
+
+// One-Sided Ranges (a...)
+// The closed range operator has an alternative form for ranges that continue as far as possible in one direction
+for name in names[2...] {
+    print(name)
+}
+// Brian
+// Jack
+
+for name in names[...2] {
+    print(name)
+}
+// Anna
+// Alex
+// Brian
+
+for name in names[..<2] {
+    print(name)
+}
+// Anna
+// Alex
+
+let range = ...5
+range.contains(7)   // false
+range.contains(4)   // true
+range.contains(-1)  // true
